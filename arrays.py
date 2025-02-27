3c06abe91204e1685ecea5170cdbf2a63b1d9798
vstlvl <zorix228337@gmail.com>
2025-02-27 18:32:58 +0300

    исправил условия в функции поиска минимума среди нечетных элементов

--- arrays.py
+++ arrays.py
@@ -5,20 +5,20 @@
 """
 
 def min_among_odd(array):
-    if(len(array) == 1 and array[0] % 2 != 0):
+    if(len(array) == 1 and array[0] % 2 == 0):
         return -1
     elif(len(array) == 1):
         return array[0]
     
     min = array[0]
-    if(array[0] % 2 != 0):
+    if(array[0] % 2 == 0):
         for i in range(1, len(array)):
-            if(array[i] % 2 == 0):
+            if(array[i] % 2 != 0):
                 min = array[i]
                 break
 
     for i in range(1, len(array)):
-        if(min > array[i] and array[i] % 2 == 0):
+        if(min > array[i] and array[i] % 2 != 0):
             min = array[i]
 
     return min
