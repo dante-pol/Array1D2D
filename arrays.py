880d4d8fb9e3507fdaa572d3a822f1d8eddc97c8
Arseny <kobalt8800@gmail.com>
2025-02-27 18:33:30 +0300

    добавлена функция max_odd_1d

--- arrays.py
+++ arrays.py
@@ -10,5 +10,16 @@ def prod_1d(list):
 
     for e in list:
         prod *= e
-        
+
     return prod
+def max_odd_1d(list):
+    max = list[0]
+    no_odds = True
+
+    for e in list:
+        if e % 2 != 0 and max < e:
+            max = e
+            no_odds = False
+
+    if not no_odds:
+        return max
