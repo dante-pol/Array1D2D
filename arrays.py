315a5844227ed11da8d21b34f3a2a8be1b0b75ad
SvinotaKiller <riritik555@gmail.com>
2025-02-27 18:34:37 +0300

    Добавлена вторая функция
    Добавлена функция по поиску произведения среди нечетных чисел.

--- arrays.py
+++ arrays.py
@@ -7,3 +7,13 @@ def prod_of_elements_more_three(array):
             sum *= number
 
     return sum
+
+def prod_among_odd_numbers(array):
+
+    sum = 1
+
+    for number in array:
+        if number % 2 != 0:
+            sum *= number
+
+    return sum
